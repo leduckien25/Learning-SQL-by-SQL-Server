ca9949b70575a3131e9a7cba64ccdc54404c2690
ldkien <duckienofficial@gmail.com>
2025-02-16 15:23:33 +0700

    insert to .sql

--- sql/sQL2025_14.sql
+++ sql/sQL2025_14.sql
@@ -25,4 +25,38 @@ FROM dbo.Orders AS o, dbo.Customers AS c, dbo.Shippers AS s
 WHERE o.CustomerID = c.CustomerID AND o.ShipVia = s.ShipperID
 	  AND c.Country <> 'USA'
 GROUP BY o.ShipCountry
-HAVING COUNT(o.OrderID) > 100
+HAVING COUNT(o.OrderID) > 100;
+
+--Excercise 6:
+SELECT DISTINCT OrderID
+FROM dbo.[Order Details]
+WHERE (UnitPrice BETWEEN 100 AND 200) OR Quantity IN (10,20);
+
+--Excercise 7:
+SELECT DISTINCT Country
+FROM dbo.Suppliers
+UNION
+SELECT DISTINCT Country
+FROM dbo.Customers;
+
+--Excercise 8:
+SELECT City, Country
+FROM dbo.Suppliers
+WHERE City = 'London'
+UNION
+SELECT ShipCity, ShipCountry
+FROM dbo.Orders
+WHERE ShipCity = 'USA'
+UNION
+SELECT City, Country
+FROM dbo.Customers
+WHERE Country = 'U%';
+
+--Excercise 9:
+SELECT DISTINCT Phone AS PhoneNumber  FROM dbo.Customers AS c
+UNION 
+SELECT DISTINCT Phone AS PhoneNumber  FROM dbo.Suppliers
+UNION 
+SELECT DISTINCT HomePhone AS PhoneNumber  FROM dbo.Employees
+UNION
+SELECT DISTINCT Phone AS PhoneNumber FROM dbo.Shippers;
